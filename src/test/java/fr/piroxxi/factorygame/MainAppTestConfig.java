da5bf454c76de927fd623415d5e8ee028b83442f
piroxxi <piroxxi@gmail.com>
2016-11-07 20:28:44 -0500

    Debut d'ajout des tests unitaires

---
+++ src/test/java/fr/piroxxi/factorygame/MainAppTestConfig.java
@@ -0,0 +1,7 @@
+package fr.piroxxi.factorygame;
+
+/**
+ * Custom configuration file to configure dependency injection for tests cases.
+ */
+public class MainAppTestConfig {
+}
---
+++ src/test/java/fr/piroxxi/factorygame/core/GameTest.java
@@ -0,0 +1,14 @@
+package fr.piroxxi.factorygame.core;
+
+import org.junit.Test;
+
+/**
+ * A Test class for the {@link fr.piroxxi.factorygame.core.Game} class.
+ */
+public class GameTest {
+
+    @Test
+    public void testStart() {
+
+    }
+}
